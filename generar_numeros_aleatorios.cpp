264ef01ed9d05418675c0f9e8b1fecac320d0e73
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-17 20:53:42 -0400

    Add files via upload
    ejercicio en clase programacion en c++  se pide realizar una table de multiplicar dado el numero por el usuario y adicional se puso el rango  segundo ejercicio, se pide al usuario que ingrese un numero y devuelve aleatoriamente los numero del 0 al 99

---
+++ generar_numeros_aleatorios.cpp
@@ -0,0 +1,30 @@
+// generar numero aleatorios 
+//objetivos escribir un programa en c++ que imprima N numeros aleatorios
+// entre 0 y 99don de n sera ingresado por el usuario
+// cuantos numero quiere generar :3 
+//3,45,12
+#include <iostream>// libreria principal 
+#include <cstdlib> // para usar la función rand()
+#include <ctime> // para usar la función time()
+using namespace std;
+
+int main()
+{
+	int n; // declarar una variable para almacenar el número de números aleatorios
+	cout << "Ingrese un numero : "; // pedir al usuario que ingrese el número
+	cin >> n; // leer el número desde la entrada estándar
+	                
+	// inicializar el generador de números aleatorios con la hora actual
+    // para que cada vez que se ejecute el programa se obtengan valores diferentes
+	srand(time(0));
+	// ejecutar un bucle desde 1 hasta n
+	// generar e imprimir los números aleatorios
+	for (int i = 1; i <= n; i++)// obtener un número aleatorio entre 0 y RAND_MAX, por ejemplo 32767
+	{
+		int r = rand() % 100;// para reducir el rango a 0 y 99, se usa el operador módulo (%)
+		cout << r << " ";// mostrar el número aleatorio en la salida estándar
+	}
+	  
+	cout << endl;// mostrar un salto de línea al final
+	return 0;
+}
---
+++ tabla_de_multiplicar.cpp
@@ -0,0 +1,23 @@
+//ejercicio 1 ta tabla de multiplicar 
+// objetivo escribir un programa en c++ que imprima la tabla de multiplicar de un numero dado por el usuario
+//utilizando el for
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int n, rango; // declarar dos variables para almacenar el número y el rango
+	cout << "Ingrese un número : "; // pedir al usuario que ingrese el numero
+	cin >> n; // leer el número desde la entrada estándar
+	cout << "Ingrese el rango: "; // pedir al usuario que ingrese el rango
+	cin >> rango; // leer el rango desde la entrada estándar
+	// se ejecuta un bucle  desde 1 hasta el rango ingresado por el usuario
+	// imprimir la tabla de multiplicar
+	for (int i = 1; i <= rango; ++i)
+	{
+		cout << n << " * " << i << " = " << n * i << endl;
+	}
+	
+	return 0;
+}
+
